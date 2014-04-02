06d34fee57df182f41b32fd2a1178f48533ace37
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-04-02 18:27:43 -0300

    fix buffer overflow

--- cfg.go
+++ cfg.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"os"
 	"strconv"
@@ -47,39 +48,58 @@ func parse(buf *bytes.Buffer) (map[string]string, error) {
 	b := bufio.NewReader(buf)
 	lc := 0
 	mp := make(map[string]string, 0)
-	for l, e := b.ReadString('\n'); e == nil; {
-		a := []rune(l)
-		if a[0] == '#' && !insideVar {
-			continue
-		}
-		for i := 0; i < len(a); i++ {
-			if !insideVar {
-				switch a[i] {
-				case '=':
-					if varName.Len() < 1 {
+	for {
+		l, e := b.ReadString('\n')
+		if e != nil {
+			break
+		} else {
+			fmt.Println("!RIOT! ", l)
+			a := []rune(l)
+			if a[0] == '#' && !insideVar {
+				continue
+			}
+			for i := 0; i < len(a); i++ {
+				fmt.Printf(" %s (%v) ", string(a[i]), lc)
+				if !insideVar {
+					fmt.Print(insideVar, " ")
+					switch a[i] {
+					case '=':
+						fmt.Print(" is '=' ")
+						if varName.Len() < 1 {
+							return nil, errors.New("Syntax error at line " + strconv.Itoa(lc) + " (" + strconv.Itoa(i) + "): " + l)
+						}
+						insideVar = true
+					case '\\', '\'', '"', '#', '^', '&':
 						return nil, errors.New("Syntax error at line " + strconv.Itoa(lc) + " (" + strconv.Itoa(i) + "): " + l)
+					default:
+						varName.WriteRune(a[i])
 					}
-					insideVar = true
-				case '\\', '\'', '"', '#', '^', '&':
-					return nil, errors.New("Syntax error at line " + strconv.Itoa(lc) + " (" + strconv.Itoa(i) + "): " + l)
-				default:
-					varName.WriteRune(a[i])
+				} else {
+					fmt.Print(insideVar)
+					if i == len(a)-1 && a[i] == '\\' {
+						varVal.WriteRune('\n')
+						fmt.Print("\n")
+						continue
+					} else if i == len(a)-1 {
+						fmt.Print("inside var will be cause because EOL\n")
+						insideVar = false
+						break
+					}
+					//TODO: parse "\" better
+					varVal.WriteRune(a[i])
 				}
-			} else {
-				if i == len(a)-1 && a[i] == '\\' {
-					varVal.WriteRune('\n')
-					continue
-				} else if i == len(a)-1 {
-					insideVar = false
-				}
-				//TODO: parse "\" better
-				varVal.WriteRune(a[i])
+			}
+			lc++
+			if !insideVar {
+				v00 := varName.String()
+				v01 := varVal.String()
+				fmt.Println("v00", v00, "v01", v01)
+				mp[strings.TrimSpace(v00)] = strings.TrimSpace(v01)
+				varName.Truncate(0)
+				varVal.Truncate(0)
 			}
 		}
-		lc++
-		if !insideVar {
-			mp[strings.TrimSpace(varName.String())] = strings.TrimSpace(varVal.String())
-		}
+
 	}
 	return mp, nil
 }
